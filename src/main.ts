09243f38cbb6b58addb04fcc3e184f2a657aac4a
Ricky Hadjirul <hadjirulricky@gmail.com>
2022-11-22 21:20:49 +0800

    Updated port

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
