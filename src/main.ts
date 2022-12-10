9b6b391955a6a658416d376259d12c83e725233e
Ricky Hadjirul <hadjirulricky@gmail.com>
2022-12-11 03:32:12 +0800

    Updated default port to 3000

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
